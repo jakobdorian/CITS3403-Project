3b20050152221ee883a4f21d59ec8f2e1b7036ce
jakobdorian <79760777+jakobdorian@users.noreply.github.com>
2022-05-21 17:55:03 +0800

    added a unit test for submitting a score

--- unit_test.py
+++ unit_test.py
@@ -1,6 +1,7 @@
 import unittest
 from app import app, db
-from app.models import User
+from app.models import User, Score
+
 
 class TestUserModel(unittest.TestCase):
     
@@ -14,9 +15,27 @@ class TestUserModel(unittest.TestCase):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
         db.create_all()
 
-    def test_drop_user(self): # TESTS DROPPING ALL TABLES FOR A USER
-        db.session.remove()
-        db.drop_all()
+    # def test_drop_user(self): # TESTS DROPPING ALL TABLES FOR A USER
+    #     db.session.remove()
+    #     db.drop_all()
+
+class TestGameModel(unittest.TestCase):
+    def test_initialize_database(self): # TESTS CREATING A DATABASE
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+        db.create_all()   
+
+    def test_new_score(self):
+        user = User(username="tester2", email="tester2@gmail.com", id=111)
+        user.set_password('imatest')
+        score = Score(user_id=111, user_score = float(80))
+        db.session.add(score)
+        db.session.commit()
+    
+    # def test_drop_user(self): # TESTS DROPPING ALL TABLES FOR A USER
+    #     db.session.remove()
+    #     db.drop_all()
+
+
 
 
 if __name__ == "__main__":
